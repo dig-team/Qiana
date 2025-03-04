bba0f8f0314ff6314de1a74e3c4fc07c9db8c42e
Simon Coumes <simon.coumes@telecom-paris.fr>
2025-03-04 15:13:45 +0100

    Better test

--- test/test_qianaExtension.py
+++ test/test_qianaExtension.py
@@ -7,7 +7,7 @@ def test_findOperand():
     from qianaExtension.formulaExtension import findOperands
     leftop, rightop, leftIndex, rightIndex = findOperands("t1,...,t2", 2)
 
-def test_schemes_clear():
+def test_schemes_basic():
     from qianaExtension.formulaExtension import getAllSchemesInstances
     from qianaExtension.signature import Signature
     lines = """
@@ -24,6 +24,28 @@ def test_schemes_clear():
     WITH $qp QUOTING $p
     """
     lines = lines.strip().splitlines()
-    sig = Signature(functions={"f":2})
+    sig = Signature()
+    allInstances = getAllSchemesInstances(lines, sig)
+    for instance in allInstances: assert "..." not in instance
+
+def test_schemes_harder():
+    from qianaExtension.formulaExtension import getAllSchemesInstances
+    from qianaExtension.signature import Signature
+    lines = """
+    # FUNCTION and PREDICATE define new base functions and predicate, respectively (elements of $F_b$ and $P_b$)
+    FUNCTION f OF ARITY 2
+    FUNCTION ff OF ARITY 3
+    PREDICATE p OF ARITY 2
+    FUNCTION c OF ARITY 0
+
+    FORMULA test
+    BODY ![X1,...,Xn] (wft(X1) &...& wft(Xn) => truth($qp(X1,...,Xn)))
+    DOT_ARITIES $p $p $p
+    RANGE $p IN BASE_PREDICATE
+    # Alternatives are BASE_PREDICATE, BASE_FUNCTION, ANY_FUNCTION, ANY_PREDICATE, QUOTED_VARIABLE
+    WITH $qp QUOTING $p
+    """
+    lines = lines.strip().splitlines()
+    sig = Signature()
     allInstances = getAllSchemesInstances(lines, sig)
     for instance in allInstances: assert "..." not in instance
