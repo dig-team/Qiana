08e4be5a218911fab4c1181f9be3ac75fcb8bfab
Simon Coumes <simon.coumes@telecom-paris.fr>
2025-03-07 10:46:52 +0100

    Remove obsolete test files for closure, dot generation, formulas, and scheme generation

--- test/test_dot.py
+++ test/test_dot.py
@@ -6,12 +6,3 @@ def test_genDotFile():
     steps = [step1, step2]
     dot = getDotFromSteps(steps)
     assert True
-
-def test_genDotImage():
-    from dotGeneration import getImgFromSteps
-    from interfaceTypes import ReasoningStep
-    step1 = ReasoningStep("1", "phi 1", "transfo", [2])
-    step2 = ReasoningStep("2", "phi 2", "input", [])
-    steps = [step1, step2]
-    img = getImgFromSteps(steps)
-    assert True
--- test/test_formulas.py
+++
@@ -1,6 +0,0 @@
-def test_Not():
-    from qianaExtension.Formulas import Formula
-    from qianaExtension.FormulaParser import parse
-
-    formula = parse("~p()")
-    assert str(formula) == "~p()"
